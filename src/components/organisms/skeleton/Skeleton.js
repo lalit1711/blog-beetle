e28ddf5bb67785b62579970abe107940d5987ff0
lalit1711 <39618229+lalit1711@users.noreply.github.com>
2021-06-23 10:28:12 +0530

    implemented aws cognito

--- src/components/organisms/skeleton/Skeleton.js
+++ src/components/organisms/skeleton/Skeleton.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../molecules/footer";
 import Navbar from "../../molecules/navbar";
+import Amplify from "aws-amplify";
+import { AuthenticatorContext } from "../../../context/authenticatorContext";
+Amplify.configure({
+	Auth: {
+		mandatorySignId: true,
+		userPoolId: "ap-southeast-1_0mytnpFdg",
+		userPoolWebClientId: "66l90vafmod7domca0ma0mjr5q"
+	}
+});
 
-function Skeleton({ children }) {
+function Skeleton({ children, ...rest }) {
+	const [user, setUser] = useState(null);
+
+	useEffect(() => {
+		userLoggedIn();
+	}, []);
+
+	const userLoggedIn = () => {
+		const user = localStorage.getItem("user");
+		if (user) {
+			setUser(JSON.parse(user));
+		} else {
+			setUser(null);
+		}
+	};
 	return (
-		<div>
+		<AuthenticatorContext.Provider value={{ user, userLoggedIn }}>
 			<Navbar />
 			<div style={{ minHeight: "37em" }}>{children}</div>
 			<Footer />
-		</div>
+		</AuthenticatorContext.Provider>
 	);
 }
 
--- src/pages/categories/Categories.js
+++ src/pages/categories/Categories.js
@@ -1,39 +1,60 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Button from "../../components/atoms/button";
 import CategoryTile from "../../components/atoms/categoryTile";
 
-function Categories() {
+function Categories(props) {
+	const [categoryList, setCategoryList] = useState([]);
+	const [selectedCategory, setSelectedCategory] = useState([]);
+
+	useEffect(() => {
+		axios.get(`/categories`).then(res => {
+			setCategoryList(res.data);
+		});
+	}, []);
+
+	function updateCategoryTile(id, action) {
+		if (action === "add") {
+			setSelectedCategory([...selectedCategory, id]);
+		} else {
+			setSelectedCategory(selectedCategory.filter(o => o !== id));
+		}
+	}
+
+	function routeToLogin() {
+		props.history.push(`/login`);
+	}
+
 	return (
 		<div className=" categories-container columns">
 			<div className="column is-2"></div>
-			<div className="column is-8">
+			<div className="column is-8 main-container">
 				<div className="columns is-multiline">
-					<div className="column is-3">
-						<CategoryTile />
-					</div>
-					<div className="column is-3">
-						<CategoryTile selected />
-					</div>
-					<div className="column is-3">
-						<CategoryTile />
-					</div>
-					<div className="column is-3">
-						<CategoryTile selected />
-					</div>
-					<div className="column is-3">
-						<CategoryTile />
-					</div>
-					<div className="column is-3">
-						<CategoryTile selected />
-					</div>
-					<div className="column is-3">
-						<CategoryTile />
-					</div>
-					<div className="column is-3">
-						<CategoryTile />
+					{categoryList.map(category =>
+						renderCategoryTile(category, updateCategoryTile, selectedCategory)
+					)}
+				</div>
+				<div className="columns">
+					<div className="column">
+						<div className="actions">
+							<Button onClick={routeToLogin}> Save</Button>
+						</div>
 					</div>
 				</div>
 			</div>
 			<div className="column is-2"></div>
+		</div>
+	);
+}
+
+function renderCategoryTile(category, updateCategoryTile, selectedCategory) {
+	return (
+		<div className="column is-3" key={category.id}>
+			<CategoryTile
+				category={category}
+				onClick={updateCategoryTile}
+				selected={selectedCategory.includes(category.id)}
+			/>
 		</div>
 	);
 }
