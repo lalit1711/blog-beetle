3337cd673f91299c79956e268b1a91fd9144b9fa
officeKoala <vy4693@gmail.com>
2021-07-11 13:32:58 +0530

    added animation

--- src/pages/landingPage/LandingPage.js
+++ src/pages/landingPage/LandingPage.js
@@ -9,6 +9,7 @@ import TrendingLoader from "../../components/organisms/loader/TrendingLoader";
 import LatestLoader from "../../components/organisms/loader/LatestLoader";
 import { Fragment } from "react";
 import SuggestedLoader from "../../components/organisms/loader/SuggestedLoader";
+import ReactLottie from "../../animation/LottieReact";
 
 function LandingPage() {
 	const [blogsList, setBlogsList] = useState([]);
@@ -24,6 +25,10 @@ function LandingPage() {
 	return (
 		<div className="landing-page">
 			<div className="container">
+				<h1 style={{ textAlign: "center", fontSize: '45px', fontWeight: "bold" }}> <span style={{ fontSize: '70px', color: '#a3ce20',textDecoration:'underline' }}>W</span>elcome to BlogBeetle</h1>
+				<ReactLottie keyIndex={1} />
+				<hr/>
+				<h1 style={{ textAlign: "center", fontSize: '25px', fontWeight: "bold" }}> <span style={{ fontSize: '40px', color: '#a3ce20',textDecoration:'underline' }}>O</span>ur trending blog posts</h1>
 				<div style={{ marginTop: "5%" }}>
 					{!load ? <TrendingBlogs blogsList={blogsList} /> : <TrendingLoader />}
 				</div>
@@ -43,6 +48,7 @@ function LandingPage() {
 					) : (
 						<SuggestedLoader />
 					)}
+					<ReactLottie />
 				</div>
 			</div>
 		</div>
