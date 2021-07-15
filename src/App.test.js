04170e9b9df129e1d1ff667706b6d1322a1e9721
officeKoala <vy4693@gmail.com>
2021-07-15 21:23:24 +0530

    testing canvas added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen ,fireEvent} from '@testing-library/react';
 import App from './App';
-
+import Login from "./pages/login/LogIn"
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Sign in/i);
+  fireEvent.click(linkElement)
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+test('should show login form', () => {
+  render(<Login />)
+  const input = screen.getByLabelText('Email')
+  // Events and assertions...
+})
